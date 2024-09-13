12328f2d08d8c5e7eeb6a28fd105626021b29b7d
Brandon Nguyen <bgn@tamu.edu>
2024-09-13 15:21:48 -0500

    Surface: Add Torus, Spring, and Vase

--- src/Surface.h
+++ src/Surface.h
@@ -114,4 +114,185 @@ public:
     }
 };
 
+class Torus : public ParametricSurface {
+    double r1 = 1;
+    double r2 = 0.3;
+
+public:
+    Torus()
+    {
+        m_uMin = 0;
+        m_uMax = 2 * PI;
+        m_vMin = 0;
+        m_vMax = 2 * PI - 0.001;
+    }
+    virtual Vec3 f(Vec2 const& p) const
+    {
+        return { (r1 + r2 * cos(p.x())) * cos(p.y()), (r1 + r2 * cos(p.x())) * sin(p.y()), -r2 * sin(p.x()) };
+    }
+
+    virtual Vec3 f_u(Vec2 const& p) const
+    {
+        return { -r2 * sin(p.x()) * cos(p.y()), -r2 * sin(p.x()) * sin(p.y()), -r2 * cos(p.x()) };
+    }
+
+    virtual Vec3 f_v(Vec2 const& p) const
+    {
+        return { -(r1 + r2 * cos(p.x())) * sin(p.y()), (r1 + r2 * cos(p.x())) * cos(p.y()), 0 };
+    }
+
+    virtual Vec3 f_uu(Vec2 const& p) const
+    {
+        return { -r2 * cos(p.x()) * cos(p.y()), -r2 * cos(p.x()) * sin(p.y()), r2 * sin(p.x()) };
+    }
+
+    virtual Vec3 f_uv(Vec2 const& p) const
+    {
+        return { r2 * sin(p.x()) * sin(p.y()), -r2 * sin(p.x()) * cos(p.y()), 0 };
+    }
+
+    virtual Vec3 f_vv(Vec2 const& p) const
+    {
+        return { -(r1 + r2 * cos(p.x())) * cos(p.y()), -(r1 + r2 * cos(p.x())) * sin(p.y()), 0 };
+    }
+};
+
+class Spring : public ParametricSurface {
+    double m_r1;
+    double m_r2;
+    double m_kh;
+
+public:
+    Spring()
+    {
+        m_uMin = 0.;
+        m_uMax = 2. * PI;
+        m_vMin = 0.;
+        m_vMax = 4. * PI;
+
+        m_r1 = 1.;
+        m_r2 = 0.3;
+        m_kh = 0.15;
+    }
+
+    virtual Vec3 f(Vec2 const& p) const
+    {
+        return {
+            (m_r1 + m_r2 * cos(p.x())) * cos(p.y()),
+            (m_r1 + m_r2 * cos(p.x())) * sin(p.y()),
+            -m_r2 * sin(p.x()) + m_kh * p.y()
+        };
+    }
+
+    virtual Vec3 f_u(Vec2 const& p) const
+    {
+        return {
+            -m_r2 * sin(p.x()) * cos(p.y()),
+            -m_r2 * sin(p.x()) * sin(p.y()),
+            -m_r2 * cos(p.x())
+        };
+    }
+
+    virtual Vec3 f_v(Vec2 const& p) const
+    {
+        return {
+            -(m_r1 + m_r2 * cos(p.x())) * sin(p.y()),
+            (m_r1 + m_r2 * cos(p.x())) * cos(p.y()),
+            m_kh
+        };
+    }
+
+    virtual Vec3 f_uu(Vec2 const& p) const
+    {
+        return {
+            -m_r2 * cos(p.x()) * cos(p.y()),
+            -m_r2 * cos(p.x()) * sin(p.y()),
+            m_r2 * sin(p.x())
+        };
+    }
+
+    virtual Vec3 f_uv(Vec2 const& p) const
+    {
+        return {
+            m_r2 * sin(p.x()) * sin(p.y()),
+            -m_r2 * sin(p.x()) * cos(p.y()),
+            0
+        };
+    }
+
+    virtual Vec3 f_vv(Vec2 const& p) const
+    {
+        return {
+            -(m_r1 + m_r2 * cos(p.x())) * cos(p.y()),
+            -(m_r1 + m_r2 * cos(p.x())) * sin(p.y()),
+            0
+        };
+    }
+};
+
+class Vase : public ParametricSurface {
+public:
+    Vase()
+    {
+        m_uMin = 0.;
+        m_uMax = 2. * PI;
+        m_vMin = -1.;
+        m_vMax = 1.;
+    }
+
+    virtual Vec3 f(Vec2 const& p) const
+    {
+        return {
+            (0.5 - 0.4 * p.y() * (1. + p.y()) * (1. - p.y())) * cos(p.x()),
+            (0.5 + 0.4 * p.y() * (1. + p.y()) * (1. - p.y())) * sin(p.x()),
+            p.y()
+        };
+    }
+
+    virtual Vec3 f_u(Vec2 const& p) const
+    {
+        return {
+            -(0.5 - 0.4 * p.y() * (1 + p.y()) * (1 - p.y())) * sin(p.x()),
+            (0.5 + 0.4 * p.y() * (1 + p.y()) * (1 - p.y())) * cos(p.x()),
+            0
+        };
+    }
+
+    virtual Vec3 f_v(Vec2 const& p) const
+    {
+        return {
+            (-0.4 + 1.2 * p.y() * p.y()) * cos(p.x()),
+            (0.4 - 1.2 * p.y() * p.y()) * sin(p.x()),
+            1.
+        };
+    }
+
+    virtual Vec3 f_uu(Vec2 const& p) const
+    {
+        return {
+            -(0.5 - 0.4 * p.y() * (1 + p.y()) * (1 - p.y())) * cos(p.x()),
+            -(0.5 + 0.4 * p.y() * (1 + p.y()) * (1 - p.y())) * sin(p.x()),
+            0.
+        };
+    }
+
+    virtual Vec3 f_uv(Vec2 const& p) const
+    {
+        return {
+            -(-0.4 + 1.2 * p.y() * p.y()) * sin(p.x()),
+            (0.4 - 1.2 * p.y() * p.y()) * cos(p.x()),
+            0.
+        };
+    }
+
+    virtual Vec3 f_vv(Vec2 const& p) const
+    {
+        return {
+            2.4 * p.y() * cos(p.x()),
+            -2.4 * p.y() * sin(p.x()),
+            0.
+        };
+    }
+};
+
 #endif
