72dc65431fa1dd5e0c3f9027dabd2609df507ed3
barnden <brandon@nguyen.vc>
2024-09-13 23:18:09 -0500

    Visualization: Update surfaces

--- visualize-py/main.py
+++ visualize-py/main.py
@@ -2,14 +2,36 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
-class H1:
+class Surface:
+    x_limits = [-1, 1]
+    y_limits = [-1, 1]
+    z_limits = [-1, 1]
+
+    winding_path_file = "points.txt"
+
+    def generate_mesh(self, N=32):
+        parametric_space = np.meshgrid(
+            np.linspace(self.u_min, self.u_max, N),
+            np.linspace(self.v_min, self.v_max, N),
+        )
+
+        return self.f(*parametric_space)
+
+    def winding_path(self):
+        with open(self.winding_path_file) as f:
+            points = [*map(lambda line: self.f(*map(lambda x: np.array([[float(x)]]), line.replace(",", "").split()[-2:])), f.readlines())]
+
+        return points
+
+class H1(Surface):
     u_min = 0
     u_max = 2. * np.pi
     v_min = 0
     v_max = 1
 
-    z_min = 0
-    z_max = 1
+    z_limits = [0, 1]
+
+    winding_path_file = "hyperboloid.txt"
 
     @staticmethod
     def f(u, v):
@@ -18,31 +40,15 @@ class H1:
             (v ** 2 - v + 0.4) * np.sin(u),
             v
         )
-    
-    def generate_mesh(self):
-        N = 32
-        parametric_space = np.meshgrid(
-            np.linspace(H1.u_min, H1.u_max, N),
-            np.linspace(H1.v_min, H1.v_max, N)
-        )
 
-        return H1.f(*parametric_space)
-    
-    def winding_path(self):
-        with open("hyperboloid.txt") as f:
-            points = [*map(lambda line: H1.f(*map(float, line.replace(",", "").split()[-2:])), f.readlines())]
-
-        return points
-
-
-class Vase:
+class Vase(Surface):
     u_min = 0
     u_max = 2. * np.pi
     v_min = -1
     v_max = 1
 
-    z_min = -1
-    z_max = 1
+    z_limits = [-1, 1]
+    winding_path_file = "vase.txt"
 
     @staticmethod
     def f(u, v):
@@ -51,23 +57,8 @@ class Vase:
             (0.5 + 0.4 * v * (1. + v) * (1. - v)) * np.sin(u),
             v
         )
-    
-    def generate_mesh(self):
-        N = 32
-        parametric_space = np.meshgrid(
-            np.linspace(Vase.u_min, Vase.u_max, N),
-            np.linspace(Vase.v_min, Vase.v_max, N)
-        )
 
-        return Vase.f(*parametric_space)
-    
-    def winding_path(self):
-        with open("vase.txt") as f:
-            points = [*map(lambda line: Vase.f(*map(float, line.replace(",", "").split()[-2:])), f.readlines())]
-
-        return points
-    
-class Spring:
+class Spring(Surface):
     u_min = 0
     u_max = 2. * np.pi
     v_min = 0
@@ -77,8 +68,8 @@ class Spring:
     r2 = 0.3
     kh = 0.15
 
-    z_min = 0
-    z_max = 2
+    z_limits = [0, 2]
+    winding_path_file = "spring.txt"
 
     @staticmethod
     def f(u, v):
@@ -87,32 +78,17 @@ class Spring:
             (Spring.r1 + Spring.r2 * np.cos(u)) * np.sin(v),
             -Spring.r2 * np.sin(u) + Spring.kh * v
         )
-    
-    def generate_mesh(self):
-        N = 32
-        parametric_space = np.meshgrid(
-            np.linspace(Spring.u_min, Spring.u_max, N),
-            np.linspace(Spring.v_min, Spring.v_max, N)
-        )
-
-        return Spring.f(*parametric_space)
-    
-    def winding_path(self):
-        with open("spring.txt") as f:
-            points = [*map(lambda line: Spring.f(*map(float, line.replace(",", "").split()[-2:])), f.readlines())]
-
-        return points
-
-class Torus:
+class Torus(Surface):
     u_min = 0
     u_max = 2. * np.pi
     v_min = 0
     v_max = 2. * np.pi - 0.001
-    z_min = -1.
-    z_max = 1.
 
     r1 = 1
     r2 = 0.3
+
+    z_limits = [-1, 1]
+    winding_path_file = "torus.txt"
 
     @staticmethod
     def f(u, v):
@@ -122,29 +98,16 @@ class Torus:
             -Torus.r2 * np.sin(u)
         )
 
-    def generate_mesh(self):
-        N = 32
-        parametric_space = np.meshgrid(
-            np.linspace(Torus.u_min, Torus.u_max, N),
-            np.linspace(Torus.v_min, Torus.v_max, N)
-        )
-
-        return Torus.f(*parametric_space)
-    
-
-    def winding_path(self):
-        with open("torus.txt") as f:
-            points = [*map(lambda line: Torus.f(*map(float, line.replace(",", "").split()[-2:])), f.readlines())]
-
-        return points
-
-class TrefoilKnot:
+class TrefoilKnot(Surface):
     u_min = 0
     u_max = 2. * np.pi
     v_min = 0
-    v_max = 2. * np.pi * 0.001
-    z_min = 0.
-    z_max = 2.
+    v_max = 2. * np.pi - 0.001
+
+    x_limits = [-3, 3]
+    y_limits = [-3, 3]
+    z_limits = [-3, 3]
+    winding_path_file = "trefoil.txt"
 
     @staticmethod
     def f(u, v):
@@ -163,34 +126,24 @@ class TrefoilKnot:
 
         e1 = np.array([0., 0., 1.])
         T = np.tensordot(e1, t, axes=[[0], [0]])
-        e1 = e1[..., None].repeat(32, 1)[..., None].repeat(32, 2)
-        e1 = (e1 - T) * t
+
+        e1 = e1[..., None, None].repeat(u.shape[0], 1).repeat(u.shape[1], 2)
+        e1 = e1 - (T * t)
         e1 /= np.linalg.norm(e1, axis=0)
 
         e2 = np.cross(t, e1, axis=0)
 
         return p0 + 0.4 * np.cos(u) * e1 + 0.4 * np.sin(u) * e2
-    
-       
-    def generate_mesh(self):
-        N = 32
-        parametric_space = np.meshgrid(
-            np.linspace(TrefoilKnot.u_min, TrefoilKnot.u_max, N),
-            np.linspace(TrefoilKnot.v_min, TrefoilKnot.v_max, N)
-        )
 
-        return TrefoilKnot.f(*parametric_space)
-
-
-for surface in [H1(), Vase(), Torus(), Spring()]:
-    X = surface.generate_mesh()
+for surface in [H1(), Vase(), Torus(), Spring(), TrefoilKnot()]: #[H1(), Vase(), Torus(), Spring()]:
+    X = surface.generate_mesh(32)
     points = surface.winding_path()
 
     fig = plt.figure()
     ax = fig.add_subplot(projection="3d")
-    ax.set_xlim3d(-1, 1)
-    ax.set_ylim3d(-1, 1)
-    ax.set_zlim3d(surface.z_min, surface.z_max)
+    ax.set_xlim3d(*surface.x_limits)
+    ax.set_ylim3d(*surface.y_limits)
+    ax.set_zlim3d(*surface.z_limits)
     ax.plot_surface(*X, linewidth=0.1, zorder=1)
     ax.plot([e[0] for e in points], [e[1] for e in points], [e[2] for e in points], markersize=10, color=[1, 0, 0], zorder=4)
     plt.show()
