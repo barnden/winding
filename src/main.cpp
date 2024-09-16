095b9b5e2124e3c72df5ffc7c4d10a6030430565
Brandon Nguyen <bgn@tamu.edu>
2024-09-16 00:22:23 -0500

    main: Remove FPE trap handler

--- src/main.cpp
+++ src/main.cpp
@@ -6,44 +6,6 @@
 #include <Eigen/Dense>
 #include <iostream>
 
-#ifdef DEBUG_FPE_TRAP
-#    include <fenv.h>
-#    include <math.h>
-#    include <signal.h>
-#    include <stdio.h>
-#    include <stdlib.h>
-
-static void
-fpe_signal_handler(int sig, siginfo_t* sip, void* scp)
-{
-    int fe_code = sip->si_code;
-
-    printf("In signal handler : ");
-
-    if (fe_code == ILL_ILLTRP)
-        printf("Illegal trap detected\n");
-    else
-        printf("Code detected : %d\n", fe_code);
-
-    abort();
-}
-
-void enable_floating_point_exceptions()
-{
-    fenv_t env;
-    fegetenv(&env);
-
-    env.__fpcr = env.__fpcr | __fpcr_trap_invalid;
-    fesetenv(&env);
-
-    struct sigaction act;
-    act.sa_sigaction = fpe_signal_handler;
-    sigemptyset(&act.sa_mask);
-    act.sa_flags = SA_SIGINFO;
-    sigaction(SIGILL, &act, NULL);
-}
-#endif
-
 int main()
 {
 #ifdef DEBUG_FPE_TRAP
